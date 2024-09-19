7538e721787c791adc42e2b71d63a37836dd47b2
Justin B. Alcorn <justin@jalcorn.net>
2024-09-19 13:18:38 -0400

    Consistent License statements

--- ical2rem.pl
+++ ical2rem.pl
@@ -3,22 +3,32 @@
 # ical2rem.pl - 
 # Reads iCal files and outputs remind-compatible files.   Tested ONLY with
 #   calendar files created by Mozilla Calendar/Sunbird. Use at your own risk.
-# Copyright (c) 2005, 2007, Justin B. Alcorn
-
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# MIT License
 # 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# Copyright (c) 2005, 2007, 2019 Justin B. Alcorn
 # 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
+# version 0.7.1 2024-09-19
+#       - Made sure license statements were consistent
+# version 0.7 2024-09-04
+#       - Added dummy _sfun to resolve Issue #8
 # version 0.6 2019-03-01
 #       - Updates to put on GitHub
 # version 0.5.2 2007-03-23
